90f17cd8fbaa959c4c21a4cf68e652bae6f18e59
Katherin-22 <katherinquiroga18@gmail.com>
2025-09-21 12:32:26 -0500

    se agrega la nueva version de las carpetas

--- 03_backend/backend/src/test/java/com/proyectosena/spring/app1/BackendApplicationTests.java
+++ 03_backend/backend/src/test/java/com/proyectosena/spring/app1/BackendApplicationTests.java
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:03_backend/backend/src/test/java/com/proyectosena/spring/app1/BackendApplicationTests.java
 package com.proyectosena.spring.app1;
-========
-package com.proyectosena.spring.app1.backend;
->>>>>>>> 1fefe9a01f0759f9d4a0fca1401cad8a0677e4a7:03_backend/backend/src/test/java/com/proyectosena/spring/app1/backend/BackendApplicationTests.java
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
