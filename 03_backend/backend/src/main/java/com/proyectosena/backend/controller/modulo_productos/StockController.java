f6a38739ee5ba146d342c471989f5b33aebe334b
Blonabe <romeoquintero0@gmail.com>
2025-09-25 17:41:04 -0500

    actualizar frontend

--- 03_backend/backend/src/main/java/com/proyectosena/backend/controller/modulo_productos/StockController.java
+++ 03_backend/backend/src/main/java/com/proyectosena/backend/controller/modulo_productos/StockController.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import com.proyectosena.backend.repository.modulo_productos.VariacionRepository;
 import com.proyectosena.backend.service.modulo_productos.GetStockService;
 import com.proyectosena.backend.service.modulo_productos.PostStockService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 
 public class StockController {
