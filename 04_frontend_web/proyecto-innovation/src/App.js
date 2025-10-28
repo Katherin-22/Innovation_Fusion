c4ee36de6b33425ba490a777617ace9a7f0aa9b6
Daniela <daniela2007lorena@gmail.com>
2025-10-28 16:56:04 -0500

    frontend web usuarios

---
+++ 04_frontend_web/proyecto-innovation/src/App.js
@@ -0,0 +1,37 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './componentes/Login'
+import RegistrarUsuarios from './componentes/RegistrarUsuarios';
+import RecuperarContraseña from './componentes/RecuperarContraseña';
+import Principal from './componentes/Principal';
+
+function App() {
+  return (
+  <Router>
+
+    <div className="App">
+     
+      <Routes>
+        {/* Ruta para el Login*/}
+        <Route  path='/login' element={<Login/>}/>
+
+        {/* Ruta para el Registro*/}
+        <Route  path='/registrarusuarios' element={<RegistrarUsuarios/>}/>
+
+        {/* Ruta para Restablecer contraseña*/}
+        <Route  path='/recuperarcontraseña' element={<RecuperarContraseña/>}/>
+
+        
+        {/* Ruta principal*/}
+        <Route  path='/principal' element={<Principal/>}/>
+      </Routes>
+
+    </div>
+   
+  </Router>
+  );
+
+}
+
+export default App;
---
+++ 04_frontend_web/proyecto-innovation/src/componentes/Principal.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+ function Principal() {
+  return (
+    <div>BIENVENIDOS</div>
+  )
+}
+export default Principal;
